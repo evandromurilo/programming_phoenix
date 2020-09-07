90155324722b1bd03f66757c8acd7d37007e295d
Evandro Murilo <evandromurilob@hotmail.com>
2020-09-07 18:42:47 -0400

    create "rumbl" project

---
+++ rumbl/lib/rumbl/accounts.ex
@@ -0,0 +1,25 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+
+  alias Rumbl.Accounts.User
+
+  def list_users do
+    [
+      %User{id: "1", name: "Murilo", username: "evandromurilo"},
+      %User{id: "2", name: "Iana", username: "ianaramos"},
+      %User{id: "3", name: "Julieta", username: "julietapb"}
+    ]
+  end
+
+  def get_user(id) do
+    Enum.find(list_users(), fn map -> map.id == id end)
+  end
+
+  def get_user_by(params) do
+    Enum.find(list_users(), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end)
+  end
+end
